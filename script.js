3305b09c641fd6541ad68e9f2f5ee4c35312d631
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-03-17 21:37:20 +0100

    operators in js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world\n");
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Rolins";
+console.log(firstName + " " + lastName);
